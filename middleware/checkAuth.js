0a7fdd35acf0ce918df8fd0169b8e81c7b4b2e19
OdilovMM <dssrinman@gmail.com>
2024-08-03 10:16:09 +0500

    auth routes for proctecting

---
+++ middleware/checkAuth.js
@@ -0,0 +1,23 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../utils");
+
+const auth = async (req, res, next) => {
+  // check headers
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("No token attached");
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // attach user to routes
+    req.user = { userId: payload.userId, name: payload.name };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("No token attached");
+  }
+};
+
+module.exports = auth
