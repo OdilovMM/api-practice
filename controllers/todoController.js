3167748792f47fa7d950bd998fb796376f0083be
OdilovMM <dssrinman@gmail.com>
2024-08-03 08:46:52 +0500

    Created controllers

---
+++ controllers/todoController.js
@@ -0,0 +1,23 @@
+const getAllTodos = async (req, res) => {
+  res.send("getAllTodos");
+};
+const getTodo = async (req, res) => {
+  res.send("getAllTodos");
+};
+const createTodo = async (req, res) => {
+  res.send("getAllTodos");
+};
+const updateTodo = async (req, res) => {
+  res.send("getAllTodos");
+};
+const deleteTodo = async (req, res) => {
+  res.send("getAllTodos");
+};
+
+module.exports = {
+  getAllTodos,
+  getTodo,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+};
